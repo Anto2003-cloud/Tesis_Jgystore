b9278e754ac04161ca3fc92ec6aa79ea486dc4d6
Anto2003-cloud <antonio041103@gmail.com>
2026-04-15 09:41:57 -0400

    Ajuste de conexión Pooler

--- database.py
+++ database.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL conectada a tu proyecto JGYSTORE-DB en Supabase
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:qjwiN48b1GwamqV4@db.zmfuanfzrrmuztucnzni.supabase.co:5432/postgres"
-
+# Nueva URL usando el Pooler de Supabase (más estable para Render)
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.zmfuanfzrrmuztucnzni:qjwiN48b1GwamqV4@aws-0-us-west-2.pooler.supabase.com:6543/postgres?sslmode=require"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
