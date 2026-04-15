f780fbce43d0506dc407b4381082a043dc5914e4
Anto2003-cloud <antonio041103@gmail.com>
2026-04-15 09:48:19 -0400

    nueva

--- database.py
+++ database.py
@@ -2,9 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Nueva URL usando el Pooler de Supabase (más estable para Render)
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.zmfuanfzrrmuztucnzni:qjwiN48b1GwamqV4@aws-0-us-west-2.pooler.supabase.com:6543/postgres?sslmode=require"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Conexión Directa optimizada para Render
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:qjwiN48b1GwamqV4@db.zmfuanfzrrmuztucnzni.supabase.co:5432/postgres"
+
+# El secreto está aquí: añadimos pool_pre_ping para que si la conexión se duerme, se despierte sola
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
