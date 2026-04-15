f6292800d6d9805bcdbb42ec20726d70bcfbf536
Anto2003-cloud <antonio041103@gmail.com>
2026-04-15 10:39:01 -0400

    prueba

--- database.py
+++ database.py
@@ -2,15 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Este link usa el puerto 6543 que es más estable para Render
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.zmfuanfzrrmuztucnzni:qjwiN48b1GwamqV4@aws-0-us-west-2.pooler.supabase.com:6543/postgres?sslmode=require"
+# Link optimizado para el Pooler de Supabase
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.zmfuanfzrrmuztucnzni:qjwiN48b1GwamqV4@aws-0-us-west-2.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    pool_pre_ping=True,
-    pool_size=10,
-    max_overflow=20
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
