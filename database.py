940c1c30760da2be506feea8e00368364dd41498
Anto2003-cloud <antonio041103@gmail.com>
2026-04-15 11:19:36 -0400

    ultima prueba

--- database.py
+++ database.py
@@ -2,18 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Este es el link optimizado para evitar el error "Network is unreachable"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.zmfuanfzrrmuztucnzni:qjwiN48b1GwamqV4@aws-0-us-west-2.pooler.supabase.com:5432/postgres?sslmode=require"
+# Hemos añadido '-pooler' después del ID del endpoint
+# Esto fuerza a Neon a usar la ruta más estable y compatible con Render
+SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_A7Xn2cSJZOhQ@ep-royal-thunder-akkujxva-pooler.us-west-2.aws.neon.tech/neondb?sslmode=require"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True,
-    connect_args={
-        "keepalives": 1,
-        "keepalives_idle": 30,
-        "keepalives_interval": 10,
-        "keepalives_count": 5,
-    }
+    pool_pre_ping=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
